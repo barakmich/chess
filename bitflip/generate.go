ecd30702b97dbf070f5031db828a06691ef5545c
Barak Michener <me@barakmich.com>
2022-07-20 14:28:02 -0700

    compile cleanly on other architectures

--- bitflip/generate.go
+++ bitflip/generate.go
@@ -1,4 +1,4 @@
 package bitflip
 
-//go:generate go run ./asm/bitflipavo.go -out bitflip.s -stubs stub.go
-//go:generate go run ./attacks/calcAttacks.go -out attacks.s -stubs attackstub.go
+//go:generate go run ./asm/bitflipavo.go -out bitflip_amd64.s -stubs bitflip_amd64.go
+//go:generate go run ./attacks/calcAttacks.go -out attacks_amd64.s -stubs attacks_amd64.go
